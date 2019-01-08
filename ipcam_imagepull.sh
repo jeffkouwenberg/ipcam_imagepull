bee6ad96d4d8ac565e5890551bd4ba768555c182
Jeff Kouwenberg <jeffkouwenberg@gmail.com>
2019-01-08 15:49:11 -0500

    Added main script file

---
+++ ipcam_imagepull.sh
@@ -0,0 +1,6 @@
+photo_filename="/path/where/you/want/to/store/image/photo_`date +%y%m%d_%H%M%S`.jpg"
+
+wget --user optional_user --password optional_pass http://<ip>:<port>/photo.jpg --output-document $photo_filename
+
+# If the image is upside down, can rotate it
+convert "$photo_filename" -rotate 180 $photo_filename
